7e21c84c0e21a728157201def9b4ce51e336b4d0
zhengyima <317958662@qq.com>
2019-07-11 18:12:34 +0800

    自动调整输入图片尺寸，输出自动还原至输入尺寸

--- main.py
+++ main.py
@@ -7,39 +7,27 @@ from run import process
 main.py
 
  How to run:
- python3 main.py
+ python main.py
 
 """
 
-def main2():
+def main():
 	dress = cv2.imread("input.png")
 
+	h = dress.shape[0]
+
+	w = dress.shape[1]
+
+	dress = cv2.resize(dress, (512,512), interpolation=cv2.INTER_CUBIC)
+
 	watermark = process(dress)
+
+	watermark =  cv2.resize(watermark, (w,h), interpolation=cv2.INTER_CUBIC)
 
 	cv2.imwrite("output.png",watermark)
 
 
-# ------------------------------------------------- main()
-def main(i):
-
-	#Read input image
-	dress = cv2.imread("j"+str(i)+".jpg")
-
-	#Process
-	watermark = process(dress)
-
-	# Write output image
-	cv2.imwrite("./out/oj"+str(i)+"out.jpg", watermark)
-
-	#Exit
 	#sys.exit()
 
 if __name__ == '__main__':
-	#main(5)
-	#main(6)
-	#main(7)
-	#main(8)
-	#main(9)
-	#main(10)
-	#main(11)
-	main2()
+	main()
