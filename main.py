d5c013932b228aef16fbc33b41f707368ac55daf
yigui.dai <yigui.dai@hex-tech.net>
2019-07-11 22:09:56 +0800

    add the parameters parser

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+# coding: utf8
 import sys
 import cv2
+import argparse
 
 from run import process
 
@@ -11,8 +13,9 @@ main.py
 
 """
 
-def main():
-	dress = cv2.imread("input.png")
+
+def main(inputpath, outpath):
+	dress = cv2.imread(inputpath)
 
 	h = dress.shape[0]
 
@@ -22,12 +25,16 @@ def main():
 
 	watermark = process(dress)
 
-	watermark =  cv2.resize(watermark, (w,h), interpolation=cv2.INTER_CUBIC)
+	watermark = cv2.resize(watermark, (w,h), interpolation=cv2.INTER_CUBIC)
 
-	cv2.imwrite("output.png",watermark)
-
+	cv2.imwrite(outputpath, watermark)
 
 	#sys.exit()
 
+
 if __name__ == '__main__':
-	main()
+	parser = argparse.ArgumentParser(description="simple deep nude script tool")
+	parser.add_argument("-i", action="store", default="input.png")
+	parser.add_argument("-o", action="store", default="output.png")
+	inputpath, outputpath = parser.parse_args().i, parser.parse_args().o
+	main(inputpath, outputpath)
