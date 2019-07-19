594757dde51802b3776bd6a066cc789933a1b45c
olektomek <olekstomek@gmail.com>
2019-07-19 19:53:04 +0200

    adding a few improvements
    - you can add more pictures as an input by giving their names using the -i or --input argument
    - you can disable automatic opening of photos using the -s or --show argument
    - adding help information to arguments

--- main.py
+++ main.py
@@ -23,29 +23,34 @@ def open_file(filepath):
         opener ="open" if sys.platform == "darwin" else "xdg-open"
         subprocess.call([opener, filepath])
 
-def main(inputpath, outpath):
-	dress = cv2.imread(inputpath)
+def main(inputpath, outpath, show):
+	automatic_show_image = show.lower() == "true"
+	if isinstance(inputpath, list):
+		for item in inputpath:
+			watermark = deep_nude_process(item)
+			cv2.imwrite("output_"+item, watermark)
+			if automatic_show_image:
+				open_file("output_"+item)
+	else:
+		watermark = deep_nude_process(inputpath)
+		cv2.imwrite(outputpath, watermark)
+		if automatic_show_image:
+			open_file(outputpath)
 
-	h = dress.shape[0]
-
-	w = dress.shape[1]
-
-	dress = cv2.resize(dress, (512,512), interpolation=cv2.INTER_CUBIC)
-
-	watermark = process(dress)
-
-	watermark = cv2.resize(watermark, (w,h), interpolation=cv2.INTER_CUBIC)
-
-	cv2.imwrite(outputpath, watermark)
-
-	open_file(outputpath)
-
-	#sys.exit()
-
+def deep_nude_process(item):
+    print('Processing {}'.format(item))
+    dress = cv2.imread(item)
+    h = dress.shape[0]
+    w = dress.shape[1]
+    dress = cv2.resize(dress, (512,512), interpolation=cv2.INTER_CUBIC)
+    watermark = process(dress)
+    watermark = cv2.resize(watermark, (w,h), interpolation=cv2.INTER_CUBIC)
+    return watermark
 
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser(description="simple deep nude script tool")
-	parser.add_argument("-i", action="store", default="input.png")
-	parser.add_argument("-o", action="store", default="output.png")
-	inputpath, outputpath = parser.parse_args().i, parser.parse_args().o
-	main(inputpath, outputpath)
+	parser.add_argument("-i", "--input", action="store", nargs = "*", default="input.png", help = "Use to enter input one or more files's name")
+	parser.add_argument("-o", "--output", action="store", default="output.png", help = "Use to enter output file name")
+	parser.add_argument("-s", "--show", action="store", default="true", help = "Use to automatically display or not display generated images")
+	inputpath, outputpath, show = parser.parse_args().input, parser.parse_args().output, parser.parse_args().show
+	main(inputpath, outputpath, show)
